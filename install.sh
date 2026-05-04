05116c55f62377660c05cc0cbc53486e7966a3fd
Muhammad Waheed <wm0297567@gmail.com>
2026-05-04 10:46:28 +0500

    fix: add libusb linking flag for owrx_connector

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ cd /tmp
 rm -rf owrx_connector
 git clone https://github.com/jketterl/owrx_connector
 cd owrx_connector && mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-lusb-1.0"
 make -j$(nproc)
 make install
 ldconfig
